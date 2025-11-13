2765b4c77b735b2f2db2e1651eb5d8a91ab9ea24
wujunwei <wjw960710@gmail.com>
2025-11-13 20:59:37 +0800

    refactor: replace css with unocss

--- aitest/src/App.vue
+++ aitest/src/App.vue
@@ -5,26 +5,11 @@ import HelloWorld from './components/HelloWorld.vue'
 <template>
   <div class="p-4">
     <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
+      <img src="/vite.svg" class="h-6em p-1.5em will-change-filter transition-filter duration-300 hover:drop-shadow-[0_0_2em_#646cffaa]" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      <img src="./assets/vue.svg" class="h-6em p-1.5em will-change-filter transition-filter duration-300 hover:drop-shadow-[0_0_2em_#42b883aa]" alt="Vue logo" />
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
--- aitest/src/components/HelloWorld.vue
+++ aitest/src/components/HelloWorld.vue
@@ -7,10 +7,10 @@ const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <h1 class="text-3.2em leading-1.1">{{ msg }}</h1>
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+  <div class="p-2em">
+    <button type="button" @click="count++" class="rounded-lg border border-transparent px-1.2em py-0.6em text-1em font-medium font-inherit bg-[#1a1a1a] cursor-pointer transition-colors hover:border-[#646cff]">count is {{ count }}</button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
@@ -19,7 +19,7 @@ const count = ref(0)
 
   <p>
     Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank" class="font-medium text-[#646cff] no-underline hover:text-[#535bf2]"
       >create-vue</a
     >, the official Vue + Vite starter
   </p>
@@ -28,14 +28,9 @@ const count = ref(0)
     <a
       href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
       target="_blank"
+      class="font-medium text-[#646cff] no-underline hover:text-[#535bf2]"
       >Vue Docs Scaling up Guide</a
     >.
   </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <p class="text-[#888]">Click on the Vite and Vue logos to learn more</p>
 </template>
-
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
