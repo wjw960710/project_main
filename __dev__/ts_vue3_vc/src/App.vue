45f8e7fed3242911595cdfdd225fca76a3d2c961
wujunwei <wjw960710@gmail.com>
2025-11-14 08:48:21 +0800

    chore: Remove default scaffold files

--- __dev__/ts_vue3_vc/src/App.vue
+++ __dev__/ts_vue3_vc/src/App.vue
@@ -1,15 +1,8 @@
+<template>
+</template>
+
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-<template>
-  <div class="p-4">
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="h-6em p-1.5em will-change-filter transition-filter duration-300 hover:drop-shadow-[0_0_2em_#646cffaa]" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="h-6em p-1.5em will-change-filter transition-filter duration-300 hover:drop-shadow-[0_0_2em_#42b883aa]" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+<style>
+</style>
