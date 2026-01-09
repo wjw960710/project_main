0747cbc9ed41c6f7de7d8a440493bac31358d3e8
wujunwei <wjw960710@gmail.com>
2026-01-09 13:17:23 +0800

    新增測試專案

---
+++ __dev__/ts_vite_tailwind_react19/src/app.tsx
@@ -0,0 +1,13 @@
+function App() {
+	return (
+		<div
+			className={
+				'flex h-screen w-full items-center justify-center text-xl font-bold text-red-700'
+			}
+		>
+			Hello ts vite tailwind react19
+		</div>
+	)
+}
+
+export default App
