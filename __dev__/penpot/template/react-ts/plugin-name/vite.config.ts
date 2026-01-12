f98adcb992f5a04d2ae157cfc7de52edb0616d2c
wujunwei <wjw960710@gmail.com>
2026-01-12 13:48:45 +0800

    pluginjs 多新增 esbuild 編譯

--- __dev__/penpot/template/react-ts/plugin-name/vite.config.ts
+++ __dev__/penpot/template/react-ts/plugin-name/vite.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from "node:path";
+import {build as esbuild} from "esbuild";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
     react(),
+    postProcessPlugin(),
   ],
   build: {
     rolldownOptions: {
@@ -24,3 +27,29 @@ export default defineConfig({
     cors: true,
   },
 })
+
+/**
+ * @desc 多編譯一次 plugin 以解決不能使用 import 語法的問題
+ */
+function postProcessPlugin () {
+  return {
+    name: 'post-process-plugin',
+    closeBundle: async () => {
+      const pluginPath = path.resolve(__dirname, 'dist/plugin.js')
+
+      console.log('正在使用 esbuild 重新封裝 plugin.js...')
+
+      await esbuild({
+        entryPoints: [pluginPath],
+        outfile: pluginPath,
+        bundle: true,
+        minify: true,
+        allowOverwrite: true,
+        format: 'iife',
+        platform: 'browser',
+      })
+
+      console.log('plugin.js 重新封裝完成！')
+    }
+  }
+}
