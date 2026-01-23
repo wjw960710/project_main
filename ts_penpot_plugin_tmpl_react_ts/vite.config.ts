37946e0abb3c83e25ceec5118fb2668d43d144b5
wujunwei <wjw960710@gmail.com>
2026-01-23 14:50:37 +0800

    chore: 新增本地與線上的 manifest name 差異化

--- ts_penpot_plugin_tmpl_react_ts/vite.config.ts
+++ ts_penpot_plugin_tmpl_react_ts/vite.config.ts
@@ -1,21 +1,23 @@
-import { defineConfig } from 'vite'
+import { defineConfig, type Plugin } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'node:path'
+import fs from 'node:fs'
 import { build as esbuild } from 'esbuild'
-import manifest from './public/manifest.json'
 
-const PROJECT_DIR = manifest.name.replace(/\s/g, '_').toLowerCase()
-const VITE_BASE = `plugin/${PROJECT_DIR}`
-const VITE_OUT_DIR = `dist/${VITE_BASE}`
+const VITE_OUT_DIR = 'dist'
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [tailwindcss(), react(), postProcessPlugin()],
-	base: `/${VITE_BASE}`,
+	plugins: [
+		tailwindcss(),
+		react(),
+		postProcessPlugin({ outDir: VITE_OUT_DIR }),
+		myPreviewServerPlugin({ outDir: VITE_OUT_DIR }),
+	],
 	resolve: {
 		alias: {
-			'@': path.resolve(__dirname, './src'),
+			'@': path.join(process.cwd(), './src'),
 		},
 	},
 	build: {
@@ -39,11 +41,11 @@ export default defineConfig({
 /**
  * @desc 多編譯一次 plugin 以解決不能使用 import 語法的問題
  */
-function postProcessPlugin() {
+function postProcessPlugin({ outDir }: { outDir: string }): Plugin {
 	return {
 		name: 'post-process-plugin',
 		closeBundle: async () => {
-			const pluginPath = path.resolve(__dirname, `${VITE_OUT_DIR}/plugin.js`)
+			const pluginPath = path.resolve(process.cwd(), `${outDir}/plugin.js`)
 
 			console.log('正在使用 esbuild 重新封裝 plugin.js...')
 
@@ -58,6 +60,25 @@ function postProcessPlugin() {
 			})
 
 			console.log('plugin.js 重新封裝完成！')
+		},
+	}
+}
+
+function myPreviewServerPlugin({ outDir }: { outDir: string }): Plugin {
+	return {
+		name: 'my-preview-server-plugin',
+		configurePreviewServer(server) {
+			server.middlewares.use('/manifest.json', async (_req, res, _next) => {
+				const manifestPath = path.join(outDir, 'manifest.json')
+				const manifest = JSON.parse(await fs.promises.readFile(manifestPath, 'utf-8'))
+
+				manifest.name = `(DEV) ${manifest.name}`
+
+				res.setHeader('Content-Type', 'application/json')
+				res.setHeader('Access-Control-Allow-Origin', '*')
+
+				res.end(JSON.stringify(manifest, null, 2))
+			})
 		},
 	}
 }
