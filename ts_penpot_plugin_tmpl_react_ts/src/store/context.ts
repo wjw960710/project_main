ca746cea36cee76f9a65448c29caf242ac94f5f0
wujunwei <wjw960710@gmail.com>
2026-01-23 17:13:49 +0800

    refactor: signal value 名稱拖尾 $

--- ts_penpot_plugin_tmpl_react_ts/src/store/context.ts
+++ ts_penpot_plugin_tmpl_react_ts/src/store/context.ts
@@ -1,4 +1,4 @@
-import {createSignal} from "react-alien-signals";
-import type {Theme} from "@penpot/plugin-types";
+import { createSignal } from 'react-alien-signals'
+import type { Theme } from '@penpot/plugin-types'
 
-export const theme = createSignal<Theme>('light')
+export const theme$ = createSignal<Theme>('light')
