57aaca574bca225cefb1abaa9a30f7defad01b38
wujunwei <wjw960710@gmail.com>
2026-02-23 14:38:43 +0800

    remove

--- __dev__/ts_vite_playground/src/main.tsx
+++
@@ -1,6 +0,0 @@
-import '@unocss/reset/eric-meyer.css'
-import 'virtual:uno.css'
-import { createRoot } from 'react-dom/client'
-import App from './app.tsx'
-
-createRoot(document.getElementById('root')!).render(<App />)
