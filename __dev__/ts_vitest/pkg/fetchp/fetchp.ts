24af94ba1e26b833437cb86cdf3c5985be35b146
wujunwei <wjw960710@gmail.com>
2026-03-18 14:06:50 +0800

    調整基礎類型

--- __dev__/ts_vitest/pkg/fetchp/fetchp.ts
+++ __dev__/ts_vitest/pkg/fetchp/fetchp.ts
@@ -1,7 +1,13 @@
+export type FetchpInput = string | URL | Request
+export type FetchpInit = RequestInit
+
 export type CreateFetchpOptions = {
 	// 默認使用原生的 fetch，傳入的話就會換成對硬的請求函數
-	engine?: (input: string | URL | Request, init?: RequestInit) => Response | Promise<Response>
-	beforeFetch?: (input: string | URL | Request, init?: RequestInit) => Promise<void> // fetch 前執行
+	engine?: (input: FetchpInput, init?: FetchpInit) => Response | Promise<Response>
+	beforeFetch?: (
+		input: FetchpInput,
+		init?: FetchpInit,
+	) => [FetchpInput, FetchpInit?] | Promise<[FetchpInput, FetchpInit?]> // fetch 前執行
 	afterFetch?: (res: Response) => Response | Promise<Response> // fetch 後執行
 }
 
@@ -9,7 +15,7 @@ export function createFetchp(options?: CreateFetchpOptions) {
 	const { engine, beforeFetch, afterFetch } = options || {}
 	const _engine = engine || fetch
 
-	return async function (input: string | URL | Request, init?: RequestInit) {
+	return async function (input: FetchpInput, init?: FetchpInit) {
 		if (beforeFetch) await beforeFetch(input, init)
 		const res = await _engine(input, init)
 		if (afterFetch) return afterFetch(res)
