aeacd971619ab14582e5b16702e2a1443ed70065
wujunwei <wjw960710@gmail.com>
2026-03-18 12:16:28 +0800

    新增中間件機制

--- __dev__/ts_vitest/pkg/fetchp/fetchp.ts
+++ __dev__/ts_vitest/pkg/fetchp/fetchp.ts
@@ -1,5 +1,33 @@
+export type FetchpMiddleware = (
+	req: { input: RequestInfo | URL; init?: RequestInit },
+	next: () => Promise<Response>,
+) => Promise<Response>
+
 export function createFetchp() {
-	return (input: RequestInfo | URL, init?: RequestInit) => {
-		return fetch(input, init)
+	const middlewares: FetchpMiddleware[] = []
+
+	const fetchp = async (input: RequestInfo | URL, init?: RequestInit) => {
+		const dispatch = async (
+			i: number,
+			req: { input: RequestInfo | URL; init?: RequestInit },
+		): Promise<Response> => {
+			if (i === middlewares.length) {
+				// 最後一個中間件執行原生 fetch
+				return fetch(req.input, req.init)
+			}
+
+			const middleware = middlewares[i]
+			return middleware(req, () => dispatch(i + 1, req))
+		}
+
+		return dispatch(0, { input, init })
 	}
+
+	// 允許動態添加中間件
+	fetchp.use = (fn: FetchpMiddleware) => {
+		middlewares.push(fn)
+		return fetchp
+	}
+
+	return fetchp
 }
